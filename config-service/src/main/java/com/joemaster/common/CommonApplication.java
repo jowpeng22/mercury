dc3f24b80d6a9b00bae341cd2fad0bb33304d99f
PENG2202 <pengzhongqiao1235@126.com>
2020-07-12 14:41:27 +0800

    配置文件修改

--- config-service/src/main/java/com/joemaster/common/CommonApplication.java
+++ config-service/src/main/java/com/joemaster/common/CommonApplication.java
@@ -2,8 +2,10 @@ package com.joemaster.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class CommonApplication {
 
 	public static void main(String[] args) {
