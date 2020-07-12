213263c422efbcd8d0e1fea3c87506ea5ba93449
PENG2202 <pengzhongqiao1235@126.com>
2020-07-12 12:03:52 +0800

    blog-service 代码修改

--- blog-service/src/main/java/com/joemaster/blogservice/BlogServiceApplication.java
+++ blog-service/src/main/java/com/joemaster/blogservice/BlogServiceApplication.java
@@ -2,8 +2,11 @@ package com.joemaster.blogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BlogServiceApplication {
 
 	public static void main(String[] args) {
