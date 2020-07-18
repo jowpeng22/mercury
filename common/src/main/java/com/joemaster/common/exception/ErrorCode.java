d11bb78bd2db1073034a8abd35ef5b4b4440eeeb
PENG2202 <pengzhongqiao1235@126.com>
2020-07-18 12:08:55 +0800

    参数异常统一处理

--- common/src/main/java/com/joemaster/common/exception/ErrorCode.java
+++ common/src/main/java/com/joemaster/common/exception/ErrorCode.java
@@ -13,8 +13,8 @@ public enum ErrorCode {
 
     OK(0, ""),
     FAIL(-1, "操作失败"),
-    BAD_PARAMETER(-3, "参数异常"),
     RPC_ERROR(-2,"远程调度失败"),
+    BAD_PARAMETER(403, "参数异常"),
     USER_NOT_FOUND(1000,"用户不存在"),
     USER_PASSWORD_ERROR(1001,"密码错误"),
     GET_TOKEN_FAIL(1002,"获取token失败"),
