8bbf1929180498190a32a75601f89ec5bf6e4192
PENG2202 <pengzhongqiao1235@126.com>
2020-07-18 12:08:14 +0800

    参数异常统一处理

--- user-service/src/main/java/com/joemaster/userservice/exception/CommonExceptionHandler.java
+++ user-service/src/main/java/com/joemaster/userservice/exception/CommonExceptionHandler.java
@@ -3,13 +3,16 @@ package com.joemaster.userservice.exception;
 import com.joemaster.common.dto.RespDTO;
 import com.joemaster.common.exception.CommonException;
 import com.joemaster.common.exception.ErrorCode;
-import com.joemaster.common.util.HttpConstant;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Create by pengzq on 2020-7-13
@@ -30,8 +33,13 @@ public class CommonExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity argumentHandleException(MethodArgumentNotValidException e) {
         RespDTO resp = new RespDTO();
+        ArrayList<String> list = new ArrayList<>();
+        List<ObjectError> allErrors = e.getBindingResult().getAllErrors();
+        if(allErrors != null && !allErrors.isEmpty()){
+            allErrors.forEach(a-> list.add(a.getDefaultMessage()));
+        }
         resp.code = ErrorCode.BAD_PARAMETER.getCode();
-        resp.error = ErrorCode.BAD_PARAMETER.getMsg();
+        resp.error = list.toString();
         return new ResponseEntity(resp, HttpStatus.OK);
     }
 }
